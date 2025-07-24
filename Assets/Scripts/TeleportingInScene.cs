c42f92cf915030e04edb77fa7a05823f59216eea
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2025-07-24 17:06:50 +0200

    Working portals with fade

--- Assets/Scripts/TeleportingInScene.cs
+++ Assets/Scripts/TeleportingInScene.cs
@@ -4,13 +4,23 @@ public class TeleportingInScene : MonoBehaviour
 {
     [SerializeField] Transform teleportTarget;
     PlayerController player;
+    Animator playerAnimator;
 
     private void Start()
     {
         player = FindFirstObjectByType<PlayerController>();
+        playerAnimator = player.GetComponent<Animator>();
     }
 
     void OnTriggerEnter(Collider other)
+    {
+        // Reset speed of the player after TP
+        player.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+        playerAnimator.SetTrigger("fade");
+        Invoke(nameof(Teleport), 0.5f);
+    }
+
+    void Teleport()
     {
         player.transform.position = teleportTarget.position;
     }
