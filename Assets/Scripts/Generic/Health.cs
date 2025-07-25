5cb85987542da79e824b5939ea09e2d2e38f7bcb
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2025-07-25 11:22:39 +0200

    Alien companion now shoots

--- Assets/Scripts/Generic/Health.cs
+++ Assets/Scripts/Generic/Health.cs
@@ -24,7 +24,7 @@ public class Health : MonoBehaviour
     void Die()
     {
         Debug.Log("crashed!");
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     public int GetHealth()
