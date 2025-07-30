f9f258134206b39a0099be8d404e9423b45c034c
Alexandra Padurean <208801745+Ale-X-a@users.noreply.github.com>
2025-07-30 22:01:44 +0200

    KeyPickup + hintSound

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
+    float pickupDelay= 3f;
+    bool isPickedUp = false;
+    public float soundVolume = 1.5f;
+    
+    public AudioClip pickupSound;
+    
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (!isPickedUp && other.CompareTag("Player"))
         {
+            if (pickupSound != null)
+            {
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position, soundVolume);
+            }
+
+            isPickedUp = true;
+            Invoke("Pickup", pickupDelay); 
             Debug.Log("Player picked up key");
-            Destroy(gameObject);
         }
+    }
+    void Pickup()
+    {
+        Debug.Log("Key picked up!");
+        Destroy(gameObject);
     }
 }
